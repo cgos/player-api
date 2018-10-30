7695f1cdfe17d426d2d88313ca69653cff183907
Christian Gosselin <gosselinchristian@gmail.com>
2018-10-29 20:55:42 -0400

    Basic project setup (#1)
    * springboot skeleton
    * full code coverage

---
+++ src/main/java/com/scoutingalpha/microservice/playerapi/controller/PlayerController.java
@@ -0,0 +1,19 @@
+package com.scoutingalpha.microservice.playerapi.controller;
+
+import com.scoutingalpha.microservice.playerapi.domain.Player;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/player")
+public class PlayerController {
+
+    @GetMapping("/one")
+    public Player player(@RequestParam(value="name", defaultValue="Wayne") String name){
+        Player p = new Player();
+        p.setFullName("Wayne");
+        return p;
+    }
+}
---
+++ src/test/java/com/scoutingalpha/microservice/playerapi/PlayerApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.scoutingalpha.microservice.playerapi;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class PlayerApiApplicationTests {
+
+	public void contextLoads() {
+	}
+
+}
